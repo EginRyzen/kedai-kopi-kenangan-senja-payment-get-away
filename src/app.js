d1760a7682911edeeccffe9c75b877e491d38009
EginRyzen <muhammadaliagil@gmail.comEgin321!>
2024-07-22 12:33:31 +0700

    feat: konfigurasi cara checkout

--- src/app.js
+++ src/app.js
@@ -7,5 +7,68 @@ document.addEventListener('alpine:init', () => {
             { id: 4, name: 'Aceh Gayo', img: '1.jpg', price: 35000 },
             { id: 5, name: 'Sumatra Madheling', img: '1.jpg', price: 40000 },
         ]
-    }))
-})
+    }));
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            //Buat Variabel untuk cek ada cart yang sama atau tidak
+            const cartItem = this.items.find((items) => items.id === newItem.id);
+
+            //Pengkondisian
+            if (!cartItem) {
+                //Jika Belum ada
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+            }else{
+                //Jika ada
+                this.items = this.items.map((item) =>{
+                    //Jika berbeda
+                    if (item.id !== newItem.id) {
+                        return item;
+                    }else{
+                        //Jika ada yang samai
+                        item.quantity++;
+                        item.total = item.price * item.quantity; 
+                        this.quantity++;
+                        this.total += newItem.price;
+                        return item;
+                    }
+                })
+            }
+
+            // console.log(this.total);
+        },
+        remove(id){
+            const cartItem = this.items.find((items) => items.id === id);
+            if (cartItem.quantity > 1) {
+                this.items = this.items.map((item) => {
+                    if (item.id !== id) {
+                        return item;
+                    }else{
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            }else if (cartItem.quantity === 1) {
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+        }
+    });
+});
+
+// Konversi ke Rupiah 
+const rupiah = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0
+    }).format(number);
+}
